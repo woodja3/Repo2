d24946c5541b60bfd9bcb31fd0745e0b482df0f7
Jeff Wood <woodja3@gmail.com>
2021-10-27 13:56:18 -0700

    Yet again, adding backupscript...

---
+++ backupscript.py
@@ -0,0 +1,23 @@
+import os
+from netmiko import ConnectHandler
+from getpass import getpass
+
+USERNAME= input("Please enter your ssh Username: ")
+PASS= getpass("Please enter your ssh Password: ")
+
+device = {
+    'ip': '192.168.1.10',
+    'username': USERNAME,
+    'password': PASS,
+    'device_type': 'cisco_iso'
+}
+
+c = ConnectHandler(**device)
+
+output - c.send_command('show run')
+
+f=open('backup.conf', 'x')
+
+f.write(output)
+
+f.close()
